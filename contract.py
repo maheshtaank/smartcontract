e6df7dec6842f32b88bc7320937a871e81563146
Mahesh Taank <48574572+maheshtaank@users.noreply.github.com>
2022-04-21 23:55:31 +0100

    update greater
    updated greeting with group and project information

--- contract.py
+++ contract.py
@@ -76,7 +76,7 @@ print("Output from greet()")
 print(greeter.functions.greet().call())
 
 nonce = W3.eth.getTransactionCount(address1)
-tx_dict = greeter.functions.setGreeting('Hello, this is Group D').buildTransaction({
+tx_dict = greeter.functions.setGreeting('Hello, this is Group D - executing smart contract for TRADE FIANCE!').buildTransaction({
   'chainId': 3,
   'gas': 1400000,
   'gasPrice': w3.toWei('40', 'gwei'),
@@ -103,4 +103,4 @@ if tx_receipt is None:
 #tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
 print("Output from greet()")
 print(greeter.functions.greet().call({"from":account1.address}))
-#'Nihao'
+#'GroupD'
