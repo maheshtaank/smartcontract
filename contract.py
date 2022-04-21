4b7ecb1f4a552b449af238416a0270d96a9af3d6
Mahesh Taank <48574572+maheshtaank@users.noreply.github.com>
2022-04-21 23:52:54 +0100

    Update contract.py

--- contract.py
+++ contract.py
@@ -76,7 +76,7 @@ print("Output from greet()")
 print(greeter.functions.greet().call())
 
 nonce = W3.eth.getTransactionCount(address1)
-tx_dict = greeter.functions.setGreeting('Nihao').buildTransaction({
+tx_dict = greeter.functions.setGreeting('Hello, this is Group D').buildTransaction({
   'chainId': 3,
   'gas': 1400000,
   'gasPrice': w3.toWei('40', 'gwei'),
